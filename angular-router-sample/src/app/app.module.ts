b5bba066168decbed5fb8081f335484b4cf935c5
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-04-05 17:54:38 -0300

    Milestone 2: Routing module

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -1,36 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
-  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent}
-];
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    // Registering the RouterModule.forRoot() in the AppModule imports array 
-    // makes the Router service available everywhere in the application.
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
     HeroListComponent,
     CrisisListComponent,
-    PageNotFoundComponent,
+    PageNotFoundComponent
   ],
   bootstrap: [ AppComponent ]
 })
